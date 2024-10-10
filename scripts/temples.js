e5e50e2beef4a107a6d549b244cb1e0dde412151
emmanuel2011 <okoyee61@gmail.com>
2024-10-11 00:29:29 +0100

    addded item

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,13 +3,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const navMenu = document.querySelector("nav ul");
   
     // Hamburger menu functionality
-    hamburger.addEventListener("click", () => {
-      if (navMenu.style.display === "block") {
-        navMenu.style.display = "none";
-        hamburger.textContent = "☰";
-      } else {
-        navMenu.style.display = "block";
-        hamburger.textContent = "✖";
-      }
+    
+  hamButton.addEventListener('click',() => {
+    navMenu.classList.toggle('show');
+    hamburger.classList.toggle('show');
+    
+      
     });
   });
