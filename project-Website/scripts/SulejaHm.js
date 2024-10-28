1bf6e9fe502417b1c6b8f48c6690deb89003ba6c
emmanuel2011 <okoyee61@gmail.com>
2024-10-28 16:05:43 +0100

    added item

---
+++ project-Website/scripts/SulejaHm.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function ()  {
+    const hamburger = document.getElementById("hamburger");
+    const navMenu = document.getElementById("nav ul");
+  
+    // Hamburger menu functionality
+    
+  hamburger.addEventListener('click',function () 
+  {
+    navMenu.classList.toggle('active');
+    
+
+    
+    
+      
+    });
+  });
