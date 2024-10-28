bf9c06ad46123a70fbe0968f251b11ebff15d669
emmanuel2011 <okoyee61@gmail.com>
2024-10-28 22:10:49 +0100

    made some changes

--- project-Website/scripts/SulejaHm.js
+++ project-Website/scripts/SulejaHm.js
@@ -14,3 +14,12 @@ document.addEventListener("DOMContentLoaded", function ()  {
       
     });
   });
+
+
+  document.addEventListener("DOMContentLoaded", (event) => {
+    const currentYear = new Date().getFullYear();
+    document.getElementById("currentyear").textContent = currentYear;
+  
+    const lastModified = document.lastModified;
+    document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
+  });
